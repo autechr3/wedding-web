5e718997d41aeca6758a80baf21cf0417f06ee84
Matt Grogan <mgrogan01@gmail.com>
2026-06-23 21:26:26 -0400

    feat: index engagement photos 47 (courtyard) + 77 (boardwalk)

--- src/assets/photos/index.ts
+++ src/assets/photos/index.ts
@@ -7,6 +7,12 @@ import p110_2000 from './LVL040526JC1-110-2000.webp';
 import p27_800 from './LVL040526JC1-27-800.webp';
 import p27_1400 from './LVL040526JC1-27-1400.webp';
 import p27_2000 from './LVL040526JC1-27-2000.webp';
+import p47_800 from './LVL040526JC1-47-800.webp';
+import p47_1400 from './LVL040526JC1-47-1400.webp';
+import p47_2000 from './LVL040526JC1-47-2000.webp';
+import p77_800 from './LVL040526JC1-77-800.webp';
+import p77_1400 from './LVL040526JC1-77-1400.webp';
+import p77_2000 from './LVL040526JC1-77-2000.webp';
 
 export interface PhotoSet {
   w800: string;
@@ -18,4 +24,6 @@ export const PHOTOS = {
   beachPortrait: { w800: p102_800, w1400: p102_1400, w2000: p102_2000 },
   embrace: { w800: p110_800, w1400: p110_1400, w2000: p110_2000 },
   archway: { w800: p27_800, w1400: p27_1400, w2000: p27_2000 },
+  courtyard: { w800: p47_800, w1400: p47_1400, w2000: p47_2000 },
+  boardwalk: { w800: p77_800, w1400: p77_1400, w2000: p77_2000 },
 } satisfies Record<string, PhotoSet>;
